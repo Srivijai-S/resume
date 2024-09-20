66c3da6548036ae22b81eec6088540b8fa68c489
Srivijai-S <srivijai.sekar@gmail.com>
2024-09-21 01:00:17 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
 import home from "./img/house.png";
 import mail from "./img/envelope-simple.png";
-import prochat from "./img/projector-screen-chart.png";
 import { useState } from "react";
 import ProfilePic from "./img/srivijaiPic.jpg";
+import gmail from "./img/microsoft-outlook-logo.png";
+import phoneimg from "./img/phone.png";
+import pdf from "./SrivijaiSResume.pdf";
+
 export default function App() {
+  // function handleClose() {
+  //   setHome(() => !maill);
+  // }
   return (
     <div className="full">
       <NavBar />
@@ -14,7 +20,6 @@ export default function App() {
   );
 }
 function NavBar() {
-  const [project, setProject] = useState(false);
   const [homee, setHome] = useState(false);
   const [maill, setMail] = useState(false);
   function handleMail() {
@@ -23,26 +28,63 @@ function NavBar() {
   function handleHome() {
     setHome(() => !homee);
   }
-  function handleProject() {
-    setProject(() => !project);
-  }
   return (
     <div className="navprofile">
       <span className="name"></span>
       <div className="navlist">
-        <img className="icon" src={home} alt="home" onClick={handleHome} />
         <img
+          style={{ cursor: "pointer" }}
           className="icon"
-          src={prochat}
-          alt="project"
-          onClick={handleProject}
+          src={home}
+          alt="home"
+          onClick={handleHome}
         />
 
-        <img className="icon" src={mail} alt="mail" onClick={handleMail} />
+        <img
+          style={{ cursor: "pointer" }}
+          className="icon"
+          src={mail}
+          alt="mail"
+          onClick={handleMail}
+        />
       </div>
 
-      {maill && <div className="MailOpen"></div>}
-      {project && <div className="ProjectOpen"></div>}
+      {maill && (
+        <div className="MailOpen">
+          <p>
+            <center>
+              <br />
+              <img
+                style={{
+                  position: "absolute",
+                  top: "25px",
+                  left: "15px",
+                  cursor: "pointer",
+                }}
+                src={phoneimg}
+                alt="phoneim"
+              />
+              +91 8870378385
+              <br /> <br />
+              <a href="mailto:srivijai.sekar@gmail.com">
+                <img
+                  style={{
+                    position: "absolute",
+                    width: "35px",
+                    top: "62px",
+                    left: "5px",
+                    cursor: "pointer",
+                  }}
+                  src={gmail}
+                  alt="mail"
+                ></img>
+              </a>
+              srivijai.sekar@gmail.com
+              <br />
+            </center>
+          </p>
+        </div>
+      )}
     </div>
   );
 }
@@ -58,27 +100,30 @@ function Profile() {
 }
 function Summary() {
   return (
-    <div>
-      <div className="summary">
-        <p>
-          <center>Hello</center>
+    <div className="summary">
+      <p>
+        <center>Hello</center>
 
-          <p className="summaryBody">Here's who I am & what I do</p>
-        </p>
+        <p className="summaryBody">Here's who I am & what I do</p>
+      </p>
+
+      <a className="resumeP" href={pdf}>
         <button className="btn_resume">
           <strong> RESUME</strong>
         </button>
-        <button className="btn_project">
-          <strong>PROJECTS</strong>
-        </button>
-        <p className="summarydetail">
-          Skilled and proficient entry-level website designer with tremendous
-          dedication to work and a demonstrated ability in customer
-          administration and satisfaction. A passionate designer can develop
-          wireframes with specialized functionality to improve the user's
-          experience.
-        </p>
-      </div>
+      </a>
+
+      <button className="btn_project">
+        <strong>PROJECTS</strong>
+      </button>
+      <p className="summarydetail">
+        &nbsp;
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Skilled
+        and proficient entry-level website designer with tremendous dedication
+        to work and a demonstrated ability in customer administration and
+        satisfaction. A passionate designer can develop wireframes with
+        specialized functionality to improve the user's experience.
+      </p>
     </div>
   );
 }
